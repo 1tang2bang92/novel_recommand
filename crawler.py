433e00ee662bd804f188fa202e95f925a8796695
1tang2bang92 <shpark0906@naver.com>
2020-12-05 20:04:19 +0900

    sql example update

--- crawler.py
+++ crawler.py
@@ -1,11 +1,27 @@
 import pymysql
 # dbconfig.py
-class MysqlController:
-    def __init__(self, host, id, pw, db_name):
-        self.conn = pymysql.connect(host=host, user= id, password=pw, db=db_name,charset='utf8')
-        self.curs = self.conn.cursor()
+connect = pymysql.connect(${{secrets.DB_USER}}, charset='utf8')
 
-    def insert_total(self,total):
-        sql = 'INSERT INTO entire_nodes (count_of_nodes) VALUES (%s)'
-        self.curs.execute(sql,(total,))
-        self.conn.commit()
+#커서 생성
+cur = connect .cursor()
+
+# sql문 실행
+sql = "select * from testTable"
+cur.execute(sql)
+
+
+# DB결과를 모두 가져올 때 사용
+rows = cur.fetchall()
+
+# 한번에 다 출력
+print(rows)
+
+# 원하는 행만 출력
+print(rows[0])
+
+# for문으로 출력
+for row in rows:
+    print(row)
+
+# 연결 해제
+connect .close()
