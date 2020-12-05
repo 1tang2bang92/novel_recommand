ce730cfd888d209906764d7348f97d96546a7728
workspace <workspace@siru.ml>
2020-12-05 22:02:48 +0900

    crawler improving

--- joara/main.py
+++ joara/main.py
@@ -5,5 +5,5 @@ from crawler import Crawler
 genres = ['fantasy', 'love', 'rofan', 'muhyep', 'fusion', 'game', 'history', 'sports', 'ltnovel', 'bl', 'gl']
 
 if __name__ == "__main__":
-    for x in range(1, 10):
-        Crawler().crawl(x, genres[0])
+    for x in range(1, 2):
+        Crawler().crawl(4)
